1f6e295fcd8e0b365e95d20dd906f0bb171f65d7
Gabriel Balestrin <gabriel.balestrin@zitrus.com.br>
2024-05-29 10:44:30 -0300

    init

---
+++ modules/scrap/src/test/java/com/balestocks/scrap/ScrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balestocks.scrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
