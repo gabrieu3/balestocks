f5d87adfe641dd0c997c279610ed2bc34bc86c33
Gabriel Balestrin <gabriel.balestrin@zitrus.com.br>
2024-11-11 15:29:42 -0300

    BS-001 webdriver automated and new div on html page

--- modules/scrap/src/main/java/com/balestocks/scrap/config/WebDriverConfig.java
+++ modules/scrap/src/main/java/com/balestocks/scrap/config/WebDriverConfig.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import java.io.IOException;
 
 @Configuration
@@ -13,11 +14,11 @@ public class WebDriverConfig {
 
     @Bean
     public WebDriver getWebDriver() throws IOException {
-        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
+        // Configura automaticamente o ChromeDriver
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
+        // Cria uma instância do WebDriver
         return new ChromeDriver(options);
     }
-
-
 }
